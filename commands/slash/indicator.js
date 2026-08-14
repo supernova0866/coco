3fc10eb59e5639d676b910f9cf612e9d48bcb0ae
Nova <supernova0866@outlook.com>
2026-08-14 16:20:15 +0530

    CodeForge: add commands/slash/indicator.js

---
+++ commands/slash/indicator.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { requireSlashPermission } = require('../../core/slashGuard');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('indicator')
+    .setDescription('Bot presence indicator commands')
+    .addSubcommand((sub) =>
+      sub
+        .setName('set')
+        .setDescription('Set the presence indicator')
+        .addStringOption((o) =>
+          o
+            .setName('status')
+            .setDescription('Presence status')
+            .setRequired(true)
+            .addChoices(
+              { name: 'Online', value: 'online' },
+              { name: 'Idle', value: 'idle' },
+              { name: 'Do Not Disturb', value: 'dnd' }
+            )
+        )
+    ),
+
+  async execute(interaction) {
+    await requireSlashPermission(interaction, 'indicator_set');
+
+    const status = interaction.options.getString('status');
+    interaction.client.user.setPresence({ status });
+    await interaction.reply(`Presence set to ${status}.`);
+  },
+};
